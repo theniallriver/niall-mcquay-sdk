f0cb08aded90dcdd8d2c2c534ddb960128f40100
Niall McQuay <niallmcquay@gmail.com>
2022-03-29 15:51:26 -0600

    Set up a fresh Typescript NPM project with ESLint formatting

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript'
+  ]
+};
